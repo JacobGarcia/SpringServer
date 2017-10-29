6c23dd2895a433badc815d87ffaf87e4ccaf7aec
JacobGarcia <rock2494@hotmail.com>
2017-10-29 01:12:54 -0600

    Finally... This shit is working

---
+++ src/com/bancomer/retohuella/ControllerClass.java
@@ -0,0 +1,15 @@
+package com.bancomer.retohuella;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/service/greeting")
+public class ControllerClass {
+	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+	public String getGreeting(@PathVariable String name) {
+		String result="Hello "+name;
+		return result;
+	}
+}
